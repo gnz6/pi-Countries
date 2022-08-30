3610b97d2a37ec2f23f41e62c531b32f92091863
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-08-30 18:41:54 -0300

    6th

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { filterByActivity, filterByContinent, getActivities, getCountries, sortByABC, sortByPopulation } from '../redux/actions';
 import Card from './Card';
 
@@ -17,34 +17,36 @@ export default function Home(){
     //localStates
     const [currentPage, setCurrentPage]= useState(0);
     const [search, setSearch]= useState("");
+    const [filter, setFilter] = useState("default");
 
     //useEffect
 
     useEffect(()=>{
         dispatch(getCountries())
         dispatch(getActivities())
+        
     },[dispatch])
-
 
     //Paging
 
     const countriesInPage= () =>{
         if(search.length === 0 && currentPage === 0) return allCountries.slice(currentPage, currentPage + 9)
-        // if(currentPage > 0) return allCountries.slice(currentPage, currentPage + 10)
         
         const filteredCountries = allCountries.filter(c=>c.name.toLowerCase().includes(search.toLowerCase()))
-        console.log(filteredCountries)
+        setFilter(currentPage.toString())
         return filteredCountries.slice(currentPage, currentPage + 10)
     }
 
     const nextPage = ()=>{
         if(allCountries.filter(c=> c.name.includes(search)).length > currentPage + 10){
+            setFilter(currentPage.toString())
             setCurrentPage(currentPage + 10)
         }
     }
 
     const prevPage = ()=>{
         if(currentPage > 0){
+            setFilter(currentPage.toString())
             setCurrentPage(currentPage - 10)
         }
     }
@@ -54,11 +56,13 @@ export default function Home(){
     const handleClick =(e)=>{
         e.preventDefault();
         dispatch(getCountries())
+        setFilter("default")
         setCurrentPage(0)
     }
 
     const handleSearch =(e)=>{
         setSearch(e.target.value)
+        setFilter(e.target.value)
         setCurrentPage(0)
     }
 
@@ -70,6 +74,7 @@ export default function Home(){
         e.preventDefault();
         dispatch(sortByABC(e.target.value))
         setOrder(e.target.value)
+        setFilter(e.target.value)
         setCurrentPage(0)
     }
 
@@ -78,6 +83,7 @@ export default function Home(){
         e.preventDefault();
         dispatch(sortByPopulation(e.target.value))
         setOrder(e.target.value)
+        setFilter(e.target.value)
         setCurrentPage(0)
     }
 
@@ -90,6 +96,7 @@ export default function Home(){
         e.preventDefault();
         dispatch(filterByContinent(e.target.value))
         setOrder(e.target.value)
+        setFilter(e.target.value)
         setCurrentPage(0)
     }
 
@@ -97,6 +104,7 @@ export default function Home(){
         e.preventDefault();
         dispatch(filterByActivity(e.target.value))
         setOrder(e.target.value)
+        setFilter(e.target.value)
         setCurrentPage(0)
     }
 
@@ -112,9 +120,8 @@ export default function Home(){
 
         <div className='orderAplhaContainer'>
                 <label>Order by Alphabet:
-                    <select name='order' onChange={handleOrderABC}>
-                        <option disabled defaultValue="ABC">ABC</option>
-                        <option value="ABC">ABC</option>
+                    <select name='order' defaultValue="ABC" onChange={handleOrderABC}>
+                        <option disabled value="ABC">ABC</option>
                         <option value= "des">  A-Z  ↓↑</option>
                         <option value= "asc">  Z-A  ↑↓ </option>
                     </select>
@@ -124,9 +131,8 @@ export default function Home(){
 
             <div className='orderPopulationContainer'>
             <label>Order by Population:
-                <select name='population' onChange={handleOrderPopulation}>
-                    <option disabled defaultValue="default">Rating</option>
-                    <option value="default">Population</option>
+                <select name='population' defaultValue="default" onChange={handleOrderPopulation}>
+                    <option value="default" disabled>Population</option>
                     <option value = "high"> More Populated </option>
                     <option value= "low"> Less populated </option>
                 </select>
@@ -148,7 +154,7 @@ export default function Home(){
         <select name='continent' defaultValue={"all"} onChange={handleContinentChange}>
             <option value="all" >Anywhere</option>
             {allContinents.map(c=> 
-               <option value={c} name="continent">{c}</option>
+               <option key={c} value={c} name="continent">{c}</option>
            )} 
         </select>
     </label> 
@@ -158,7 +164,7 @@ export default function Home(){
         <select name='activities' defaultValue={"all"} onChange={handleActivityChange}>
             <option value="all" >Anywhere</option>
             {allActivities.map(a=> 
-               <option value={a.name} name="continent">{a.name}</option>
+               <option key={a.id} value={a.name} name="continent">{a.name}</option>
            )} 
         </select>
     </label> 
@@ -177,20 +183,24 @@ export default function Home(){
 
     <div className='cardsContainer'>
         {!countriesInPage()[0]?
-        <h1>LOADINGGG</h1>:
+        <h1>LOADINGGG</h1>
+        :
         allCountries === "error"?
         <NavLink to={"/"}>
             <button>return</button>
-        </NavLink>:
+        </NavLink>
+        :
 
         countriesInPage()?.map(c=>{
             return(
-                <div key={c.id} className= "gridCountry">
+                <div key={c.id} id={c.id} className= "gridCountry">
                     <NavLink to={`/home/${c.id}`}
                     
                     >
                     
                     <Card
+                    key={c.id}
+                    id={c.id}
                     name={c.name}
                     flag={c.flag}
                     continent={c.continent}
