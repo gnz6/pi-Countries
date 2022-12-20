63a06b010cc818d75a950af5b991f1e392d22ce4
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-12-19 23:07:17 -0300

    bug fixes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,69 +3,70 @@ const axios = require("axios");
 const router = Router();
 const Sequelize = require("sequelize");
 const { where } = require('sequelize');
-const {substring} = Sequelize.Op;
-const {Country, Activity} = require("../db");
+const { substring } = Sequelize.Op;
+const { Country, Activity } = require("../db");
 const restCountries = "https://restcountries.com/v3/all";
 
 
 //Controllers
 
-router.get("/countries", async(req, res)=>{
-    const {name} = req.query;
+router.get("/countries", async (req, res) => {
+    const { name } = req.query;
     try {
         const countries = await Country.findAll({
-            include:[{
+            include: [{
                 model: Activity,
-                attributes:["name","dificulty","duration","season"],
-                through:{attributes:[]}
-               }]
-    })
+                attributes: ["name", "dificulty", "duration", "season"],
+                through: { attributes: [] }
+            }]
+        })
 
-    if(!name && !countries.length){
-        const url = await axios.get(restCountries);
-    const resp = url.data;
-    try {
-        let results = resp.map(c=>{
-            return{
-                name: c.name.common?c.name.common: c.name.official,
-                id: c.cca3? c.cca3: c.cioc,
-                flag: c.flags[0]?c.flags[0]: c.flags[1],
-                continent: c.continents?c.continents[0]: "no continent registered",
-                capital: c.capital? c.capital[0]: "no capital registered",
-                subregion: c.subregion? c.subregion: "no subregion registered",
-                area: c.area? c.area: 0,
-                population: c.population?c.population: 0
+        if (!name && !countries.length) {
+            const url = await axios.get(restCountries);
+            const resp = url.data;
+            try {
+                let results = resp.map(c => {
+                    return {
+                        name: c.name.common ? c.name.common : c.name.official,
+                        id: c.cca3 ? c.cca3 : c.cioc,
+                        flag: c.flags[0] ? c.flags[0] : c.flags[1],
+                        continent: c.continents ? c.continents[0] : "no continent registered",
+                        capital: c.capital ? c.capital[0] : "no capital registered",
+                        subregion: c.subregion ? c.subregion : "no subregion registered",
+                        area: c.area ? c.area : 0,
+                        population: c.population ? c.population : 0,
+                    }
+                })
+
+                const response = await Country.bulkCreate(results);
+                return res.status(200).send(response)
+
+            } catch (error) {
+                console.log(error)
             }
-        })
-    
-        const response =  await Country.bulkCreate(results);
-        return res.status(200).send(response)
-
-    } catch (error) {
-        console.log(error)
-    }
-    }
-    if(name && countries.length){
-        try{
-        const country = await Country.findAll({
-            where: {
-                name :{[substring]: name}},
-            include:[{
-                model: Activity,
-                attributes:["name","dificulty","duration","season"],
-                through:{attributes:[]}
-               }]
-        })
-        country.length? res.status(200).send(country):
-        res.status(400).send("No country matches that name")
-    }
-        catch(error){
-        return res.status(400).send("No country matches that name")
         }
-    }if(!name && countries.length){
-        res.status(200).send(countries)
-    }
-        
+        if (name && countries.length) {
+            try {
+                const country = await Country.findAll({
+                    where: {
+                        name: { [substring]: name }
+                    },
+                    include: [{
+                        model: Activity,
+                        attributes: ["name", "dificulty", "duration", "season"],
+                        through: { attributes: [] }
+                    }]
+                })
+                country.length ? res.status(200).send(country) :
+                    res.status(400).send("No country matches that name")
+            }
+            catch (error) {
+                return res.status(400).send("No country matches that name")
+            }
+        } if (!name && countries.length) {
+            res.status(200).send(countries)
+        }
+
     } catch (error) {
         console.log(error)
     }
@@ -73,77 +74,76 @@ router.get("/countries", async(req, res)=>{
 
 
 
-router.post("/activity", async(req, res)=>{
-    const {name, dificulty, duration, season, countryName} = req.body;
-    if(!name||!dificulty|!duration||!season||!countryName)return res.status(404).send("Missing parameters")
-   
-   try {
-    const newActivity =await Activity.create({name, dificulty, duration, season, countryName});
-   
-    const findCountry = await Country.findAll({
-        where:{name: countryName}
-    })
+router.post("/activity", async (req, res) => {
+    const { name, dificulty, duration, season, countryName } = req.body;
+    if (!name || !dificulty | !duration || !season || !countryName) return res.status(404).send("Missing parameters")
 
-    await newActivity.addCountries(findCountry)
+    try {
+        const newActivity = await Activity.create({ name, dificulty, duration, season, countryName });
 
-    return res.status(200).send(newActivity)
-    
+        const findCountry = await Country.findAll({
+            where: { name: countryName }
+        })
 
-   } catch (error) {
-    console.log(error)
-    return res.status(400).send(error)
-   }
+        await newActivity.addCountries(findCountry)
+
+        return res.status(200).send(newActivity)
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send(error)
+    }
 
 })
 
 
 
-router.get("/countries/:id", async(req, res)=>{
-    const {id} = req.params;
+router.get("/countries/:id", async (req, res) => {
+    const { id } = req.params;
     try {
         const country = await Country.findByPk(id,
-            {include:[{
-                model: Activity,
-                attributes:["name","dificulty","duration","season"],
-                through:{attributes:[]}
-               }]
+            {
+                include: [{
+                    model: Activity,
+                    attributes: ["name", "dificulty", "duration", "season"],
+                    through: { attributes: [] }
+                }]
             })
-            return res.status(200).send(country)
+        return res.status(200).send(country)
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
-router.get("/activity", async(req,res)=>{
+router.get("/activity", async (req, res) => {
     try {
         const activities = await Activity.findAll({
-            include:[{
+            include: [{
                 model: Country,
-                attributes:["name"],
-                through:{attributes:[]}
-               }]        
-            })
+                attributes: ["name"],
+                through: { attributes: [] }
+            }]
+        })
         return res.status(200).send(activities)
-        
+
     } catch (error) {
-       return res.status(400).send("No activities stored")
+        return res.status(400).send("No activities stored")
     }
 })
 
-router.get("/continents", async(req, res)=>{
+router.get("/continents", async (req, res) => {
     try {
         const countries = await Country.findAll();
-        const continents = new Set( countries.map((c)=> c.continent))
+        const continents = new Set(countries.map((c) => c.continent))
         let allContinents = [];
-        continents.forEach(c=> allContinents.push(c))      
+        continents.forEach(c => allContinents.push(c))
 
         res.status(200).send(allContinents)
     } catch (error) {
         res.status(400).send(error)
     }
 })
-
-
 
 
 module.exports = router;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import { useDispatch, useSelector} from "react-redux";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
 import { filterByActivity, filterByContinent, getActivities, getContinents, getCountries, sortByABC, sortByPopulation } from '../redux/actions';
 import Card from './Card';
@@ -12,58 +12,59 @@ import Footer from "./Footer"
 
 
 
-export default function Home(){
+export default function Home() {
     document.title = "GlobeWiki"
 
-    
+
     const dispatch = useDispatch();
-    const allCountries = useSelector(state=> state.countries)
-    const allActivities = useSelector(state=> state.activities)
-    const allContinents = useSelector(state=> state.continents)
+    const allCountries = useSelector(state => state.countries)
+    const allActivities = useSelector(state => state.activities)
+    const allContinents = useSelector(state => state.continents)
 
     //localStates
-    const [currentPage, setCurrentPage]= useLocalStorage("currentPage", "");
-    const [search, setSearch]= useLocalStorage("search","");
+    const [currentPage, setCurrentPage] = useLocalStorage("currentPage", "");
+    const [search, setSearch] = useLocalStorage("search", "");
 
     //useEffect
 
-    useEffect(()=>{
-        
-        dispatch(getCountries())
+    useEffect(() => {
         dispatch(getActivities())
         dispatch(getContinents())
-       
-        
-    },[dispatch])
+        dispatch(getCountries())
+
+
+    }, [dispatch])
+
+
 
     //Paging
 
-    const countriesInPage= () =>{
-        if(search.length === 0 && currentPage === 0) return allCountries.slice(currentPage, currentPage + 9)
-        let filteredCountries = allCountries.filter(c=>c.name.toLowerCase().includes(search.toLowerCase()))
-        
-        if(currentPage === 0){
+    const countriesInPage = () => {
+        if (search.length === 0 && currentPage === 0) return allCountries.slice(currentPage, currentPage + 9)
+        let filteredCountries = allCountries.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+
+        if (currentPage === 0) {
             return filteredCountries.slice(currentPage, currentPage + 9)
         }
         return filteredCountries.slice(currentPage, currentPage + 10)
 
     }
 
-    const nextPage = ()=>{
-        if(allCountries.filter(c=> c.name.includes(search)).length > currentPage + 10){
+    const nextPage = () => {
+        if (allCountries.filter(c => c.name.includes(search)).length > currentPage + 10) {
             setCurrentPage(currentPage + 10)
         }
     }
 
-    const prevPage = ()=>{
-        if(currentPage > 0){
+    const prevPage = () => {
+        if (currentPage > 0) {
             setCurrentPage(currentPage - 10)
         }
     }
 
 
     //searchBar && refresh button
-    const handleClick =(e)=>{
+    const handleClick = (e) => {
         e.preventDefault();
         dispatch(getCountries())
         setSearch("")
@@ -72,31 +73,39 @@ export default function Home(){
         setFilterActivity("all")
         setFilterContinent("all")
         setCurrentPage(0)
+        document.getElementsByClassName("sb")[0].value = ""
+        document.getElementById("popOrder").value = ""
+        document.getElementById("abcOrder").value = ""
+        document.getElementById("activitySelect").value = "all"
+        document.getElementById("filterSelect").value = "all"
+
     }
 
-    const handleSearch =(e)=>{
+    const handleSearch = (e) => {
         setSearch(e.target.value)
         setCurrentPage(0)
     }
-    
 
-    
-    
-    const[orderABC, setOrderABC] = useLocalStorage("orderABC", "")
 
-    const handleOrderABC = (e)=>{
+
+
+    const [orderABC, setOrderABC] = useLocalStorage("orderABC", "")
+
+    const handleOrderABC = (e) => {
         dispatch(sortByABC(e.target.value))
         setOrderABC(e.target.value)
+        document.getElementById("popOrder").value = ""
         setOrderPopulation("")
         setCurrentPage(0)
     }
 
-    const[orderPopulation, setOrderPopulation] = useLocalStorage("orderPopulation", "")
+    const [orderPopulation, setOrderPopulation] = useLocalStorage("orderPopulation", "")
 
 
-    const handleOrderPopulation = (e)=>{
+    const handleOrderPopulation = (e) => {
         dispatch(sortByPopulation(e.target.value))
         setOrderPopulation(e.target.value)
+        document.getElementById("abcOrder").value = ""
         setOrderABC("")
         setCurrentPage(0)
     }
@@ -104,170 +113,194 @@ export default function Home(){
 
     //filters
 
-    const [filterContinent, setFilterContinent] = useLocalStorage("filterContinent","")
+    const [filterContinent, setFilterContinent] = useLocalStorage("filterContinent", "")
 
 
-    const handleContinentChange =(e)=>{
+    const handleContinentChange = (e) => {
         dispatch(filterByContinent(e.target.value))
         setFilterContinent(e.target.value)
+        document.getElementById("activitySelect").value = "all"
         setFilterActivity("all")
+
         setCurrentPage(0)
     }
 
 
-    const [filterActivity, setFilterActivity] = useLocalStorage("filterActivity","")
+    const [filterActivity, setFilterActivity] = useLocalStorage("filterActivity", "")
 
 
-    const handleActivityChange =(e)=>{
+    const handleActivityChange = (e) => {
         dispatch(filterByActivity(e.target.value))
         setFilterActivity(e.target.value)
+        document.getElementById("filterSelect").value = "all"
         setFilterContinent("all")
+
         setCurrentPage(0)
     }
 
 
-
-
-  return (
-<div className='homeContainer'>
-    <div className='homeNav'>
-    <div className='titleContainer'>
-      <NavLink to="/home" className="navTitle">
-
-        <h1 className='titleText'>G </h1>
-        <img className='titleImg' src='https://pngpress.com/wp-content/uploads/2020/08/uploads_compass_compass_PNG25585.png' alt='logo'/>
-        <h1 className='titleText'> !</h1>
-        <p className='titlesubText'>GlobeWiki</p>
-      </NavLink>
-<div className='sbContainer'>
-            <input className='sb' onChange={handleSearch} placeholder="Search Countries..."/>
-        </div>  
-    </div>
-    <div className='allBarsContainer'>
-        
-
-        <div className='arrows'>
-                    <button className='navPagingButton' onClick={prevPage} disabled={currentPage <= 0}> ← </button>
-            <button className='numButton'>{currentPage === 0 ? 0: currentPage.toString().length === 2? currentPage.toString()[0]: currentPage.toString()[0]+currentPage.toString()[1]}</button>
-                    <button className='navPagingButton' onClick={nextPage}> → </button>
-                </div>
-
-    <div className='orderFilter'>
-
-        <div className='orderContainer'>
-       
-            <label classname="homeLabel">
-            <h3>Order By</h3>
-                    <select className="homeSelect" name='order' defaultValue={""} onChange={handleOrderABC}>
-                        <option value={""} >ABC</option>
-                        <option value= "des">  A-Z  ↓↑</option>
-                        <option value= "asc">  Z-A  ↑↓ </option>
-                    </select>
-                </label>
-
-
-           
-
-            <label classname="homeLabel">
-                <select className="homeSelect" name='population' defaultValue={""} onChange={handleOrderPopulation}>
-                    <option value={""} >Population</option>
-                    <option value = "high"> More Populated </option>
-                    <option value= "low"> Less populated </option>
-                </select>
-            </label>
-        </div>
-       
-        <div className='filterContainer'>
-        <label classname="homeLabel"> 
-        <h3>Filter By</h3>
-        <select className="homeSelect" name='continent' defaultValue={"all"} onChange={handleContinentChange}>
-            <option value="all" >Continent</option>
-            {allContinents.map(c=> 
-               <option key={c} value={c} name="continent">{c}</option>
-           )} 
-        </select>
-    </label> 
-
-   
-    <label classname="homeLabel"> 
-        <select className="homeSelect" name='activities' defaultValue={"all"} onChange={handleActivityChange}>
-            <option value="all" >Activity Type</option>
-            {allActivities.map(a=> 
-               <option key={a.id} value={a.name} name="continent">{a.name}</option>
-           )} 
-        </select>
-    </label> 
-</div>
-
-        
-</div> 
-
-      
-
-        <div className='pagingContainerBar'>
-            <NavLink to={"/createActivity"}><button className='pagingButtonBar'>Create Activity</button></NavLink>
-            <button className='pagingButtonBar' onClick={handleClick}>Refresh Countries ↺ </button>
-        </div>
-
-           
-    </div>
-
-    <Footer/>
-    </div>
-      
-
-
-
-    <div className='cardsContainer'>
-        {!countriesInPage()[0]?
-        <div onClick={()=>setSearch("")}>
-            <Loader/>
-        </div>
-        :
-        allCountries === "error"?
-            <Return/>
-        :
-
-        countriesInPage()?.map(c=>{
-            return(
-                <div key={c.id} id={c.id} >
-                    <NavLink to={`/home/${c.id}`}
-                    className="gridCountry"
-                    >
-                    
-                    <Card 
-                    key={c.id}
-                    id={c.id}
-                    name={c.name}
-                    flag={c.flag}
-                    continent={c.continent}
-                    population={c.population? 
-                        c.population.toString().length ===10 ? `${c.population.toString()[0]}${c.population.toString()[1]}${c.population.toString()[2]}${c.population.toString()[3]}.${c.population.toString()[4]} Million`:
-                        c.population.toString().length ===9 ? `${c.population.toString()[0]}${c.population.toString()[1]}${c.population.toString()[2]}.${c.population.toString()[3]} Million`:
-                        c.population.toString().length ===8 ? `${c.population.toString()[0]}${c.population.toString()[1]}.${c.population.toString()[2]} Million`:
-                        c.population.toString().length ===7 ? `${c.population.toString()[0]}.${c.population.toString()[1]} Million`:
-                    c.population: 0}
-     
-                    />
-
-                    </NavLink>
-                
-                </div>
-            )
-        })
+    const emptySb = (e) => {
+        e.preventDefault()
+        document.getElementsByClassName("sb")[0].value = ""
+        setSearch("")
     }
-    <div className='pagingContainer'>
-    <button className='pagingButton' onClick={prevPage}> ← Previous Page</button>
-    <button className='numButton'>{currentPage === 0 ? 0: currentPage.toString().length === 2? currentPage.toString()[0]: currentPage.toString()[0]+currentPage.toString()[1]}</button>
-    <button className='pagingButton' onClick={nextPage}>Next Page → </button>
-
-    </div> 
-
-    </div>
- 
 
 
 
-    </div>
-  )
+
+
+
+    return (
+        <div className='homeContainer'>
+            <div className='homeNav'>
+                <div className='titleContainer'>
+                    <NavLink to="/home" className="navTitle">
+
+                        <h1 className='titleText'>G </h1>
+                        <img className='titleImg' src='https://pngpress.com/wp-content/uploads/2020/08/uploads_compass_compass_PNG25585.png' alt='logo' />
+                        <h1 className='titleText'> !</h1>
+                        <p className='titlesubText'>GlobeWiki</p>
+                    </NavLink>
+                    <div className='sbContainer'>
+                        <input className='sb' onChange={handleSearch} placeholder="Search Countries..." name='searchBar' />
+                        <button className='sbButton' onClick={emptySb}>X</button>
+                    </div>
+                </div>
+                <div className='allBarsContainer'>
+
+
+                    <div className='arrows'>
+                        <button className='navPagingButton' onClick={prevPage} > ← </button>
+                        <button className='numButton'>{parseInt(currentPage === 0 ? 0 : currentPage.toString().length === 2 ? currentPage.toString()[0] : currentPage.toString()[0] + currentPage.toString()[1]) + 1}</button>
+                        <button className='navPagingButton' onClick={nextPage}> → </button>
+                    </div>
+
+                    <div className='orderFilter'>
+
+                        <div className='orderContainer'>
+
+                            <label >
+                                <h3>Order By</h3>
+                                <select className="homeSelect" id='abcOrder' name='order' defaultValue={""} onChange={handleOrderABC}>
+                                    <option value={""} >ABC</option>
+                                    <option value="des">  A-Z  ↓↑</option>
+                                    <option value="asc">  Z-A  ↑↓ </option>
+                                </select>
+                            </label>
+
+
+
+
+                            <label >
+                                <select className="homeSelect" id='popOrder' name='population' defaultValue={""} onChange={handleOrderPopulation}>
+                                    <option value={""} >Population</option>
+                                    <option value="high"> More Populated </option>
+                                    <option value="low"> Less populated </option>
+                                </select>
+                            </label>
+                        </div>
+
+                        <div className='filterContainer'>
+                            <label >
+                                <h3>Filter By</h3>
+                                <select className="homeSelect" id='filterSelect' name='continent' defaultValue={"all"} onChange={handleContinentChange}>
+                                    <option value="all" >Continent</option>
+                                    {allContinents.map(c =>
+                                        <option key={c} value={c} name="continent">{c}</option>
+                                    )}
+                                </select>
+                            </label>
+
+
+                            <label>
+                                <select className="homeSelect" id='activitySelect' name='activities' defaultValue={"all"} onChange={handleActivityChange}>
+                                    <option value="all" >Activity Type</option>
+                                    {allActivities.map(a =>
+                                        <option key={a.id} value={a.name} name="continent">{a.name}</option>
+                                    )}
+                                </select>
+                            </label>
+                        </div>
+
+
+                    </div>
+
+
+
+                    <div className='pagingContainerBar'>
+                        <NavLink to={"/createActivity"}><button className='pagingButtonBar'>Create Activity</button></NavLink>
+                        <button className='pagingButtonBar' onClick={handleClick}>Refresh Countries ↺ </button>
+                    </div>
+
+
+                </div>
+
+                <Footer />
+            </div>
+
+
+
+
+            <div className='cardsContainer'>
+                <div className='cardsContainer2'>
+
+                    {!countriesInPage()[0] ?
+                        <div onClick={emptySb}>
+                            <Loader />
+                        </div>
+                        :
+                        allCountries === "error" ?
+                            <Return />
+                            :
+
+                            countriesInPage()?.map(c => {
+                                return (
+                                    <div key={c.id} id={c.id} >
+                                        <NavLink to={`/home/${c.id}`}
+                                            className="gridCountry"
+                                        >
+
+                                            <Card
+                                                key={c.id}
+                                                id={c.id}
+                                                name={c.name}
+                                                flag={c.flag}
+                                                continent={c.continent}
+                                                population={c.population ?
+                                                    c.population.toString().length === 10 ? `${c.population.toString()[0]}${c.population.toString()[1]}${c.population.toString()[2]}${c.population.toString()[3]}.${c.population.toString()[4]} Million` :
+                                                        c.population.toString().length === 9 ? `${c.population.toString()[0]}${c.population.toString()[1]}${c.population.toString()[2]}.${c.population.toString()[3]} Million` :
+                                                            c.population.toString().length === 8 ? `${c.population.toString()[0]}${c.population.toString()[1]}.${c.population.toString()[2]} Million` :
+                                                                c.population.toString().length === 7 ? `${c.population.toString()[0]}.${c.population.toString()[1]} Million` :
+                                                                    c.population.toString().length === 6 ? `${c.population.toString()[0]}${c.population.toString()[1]}${c.population.toString()[2]}.${c.population.toString()[3]}${c.population.toString()[4]}${c.population.toString()[5]} ` :
+                                                                        c.population.toString().length === 5 ? `${c.population.toString()[0]}${c.population.toString()[1]}.${c.population.toString()[2]}${c.population.toString()[3]}${c.population.toString()[4]} ` :
+                                                                            c.population.toString().length === 4 ? `${c.population.toString()[0]}.${c.population.toString()[1]}${c.population.toString()[2]}${c.population.toString()[3]} ` :
+                                                                                c.population.toString().length === 3 ? `${c.population} ` :
+                                                                                    c.population.toString().length < 3 ? `${c.population} ` :
+                                                                                        c.population : 0
+
+                                                }
+
+                                            />
+
+                                        </NavLink>
+
+                                    </div>
+                                )
+                            })
+                    }
+                    <div className='pagingContainer'>
+                        <button className='pagingButton' onClick={prevPage}> ← Previous Page</button>
+                        <button className='numButton'>{parseInt(currentPage === 0 ? 0 : currentPage.toString().length === 2 ? currentPage.toString()[0] : currentPage.toString()[0] + currentPage.toString()[1]) + 1}</button>
+                        <button className='pagingButton' onClick={nextPage}>Next Page → </button>
+
+                    </div>
+
+                </div>
+
+
+
+
+            </div>
+        </div>
+    )
 }
